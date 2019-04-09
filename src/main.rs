4388b5ac218dbd7ccddc9721d563711f1b7c13dc
Tom <tommyp1ckles@users.noreply.github.com>
2019-04-09 08:15:46 -0700

    Start working on vertex lists

--- src/main.rs
+++ src/main.rs
@@ -32,18 +32,22 @@ use image::RGB;
 use image::png::PNGEncoder;
 use std::fs::File;
 
-use std::io;
+use std::{
+    io,
+    fs
+};
 
 use cgmath::{
     Vector3,
+    Matrix3
 };
 
 use std::sync::{Arc};
 
-const ANTI_ALIASING_SAMPLE: i32 = 64;
+const ANTI_ALIASING_SAMPLE: i32 = 8;
 
-const IMG_WIDTH: usize = 2560;
-const IMG_HEIGHT: usize = 1440;
+const IMG_WIDTH: usize = 800;
+const IMG_HEIGHT: usize = 400;
 const T_MAX: f32 = 10000.0;
 const T_MIN: f32 = 0.001;
 const MAX_RECURSION_SIZE: i32 = 100;
@@ -57,6 +61,51 @@ fn write_image(filename: &String, data: &[u8], width: u32, height: u32) -> io::R
         height,
         RGB(8)
     )
+}
+
+use std::f32;
+
+fn parse_vertex(s: &str) -> Vector3<f32> {
+    let mut iter = s.split(",");
+    let x_str = iter.next().expect("x column should exist");
+    let y_str = iter.next().expect("y column should exist");
+    let z_str = iter.next().expect("z column should exist");
+    println!("=> {} {} {}", x_str, y_str, z_str);
+    let x: f32 = x_str.trim().parse().expect("x should parse");
+    let y: f32 = y_str.trim().parse().expect("y should parse");
+    let z: f32 = z_str.trim().parse().expect("z should parse");
+    Vector3::new(x, y, z)
+}
+
+fn read_vertex_file(filename: String, objs: &mut Vec<Box<Visible>>) {
+    let contents = fs::read_to_string(filename).expect("Read in string");
+    let mut v0: Vector3<f32>;
+    let mut v1: Vector3<f32>;
+    let mut v2: Vector3<f32>;
+    let mut line_iter = contents.split("\n");
+    loop {
+        let l0 = line_iter.next();
+        let l1 = line_iter.next();
+        let l2 = line_iter.next();
+        match l0 {
+            Some(v) => v0 = parse_vertex(v),
+            None => break,
+        }
+        match l1 {
+            Some(v) => v1 = parse_vertex(v),
+            None => break,
+        }
+        match l2 {
+            Some(v) => v2 = parse_vertex(v),
+            None => break,
+        }
+        v0 += Vector3::new(1.0, 1.0, -3.0);
+        v1 += Vector3::new(1.0, 1.0, -3.0);
+        v2 += Vector3::new(1.0, 1.0, -3.0);
+        objs.push(Box::new(
+            Triangle::new(v0, v1, v2, 1)
+        ))
+    }
 }
 
 fn unit_vector(v: &Vector3<f32>) -> Vector3<f32> {
@@ -146,15 +195,16 @@ fn main() {
     ));
 
     let mut objects: Vec<Box<Visible>> = Vec::new();
-    /*objects.push(Box::new(
+    //read_vertex_file(String::from("vertices.txt"), &mut objects);
+    objects.push(Box::new(
         Sphere::new(
-            Vector3::new(0.0, 0.0, -1.0),
+            Vector3::new(0.0, 1.0, -3.0),
             0.5,
             0
         )
-    ));*/
+    ));
 
-    objects.push(Box::new(
+    /*objects.push(Box::new(
         Sphere::new(
             Vector3::new(-1.0, 0.0, -1.0),
             0.5,
@@ -176,13 +226,43 @@ fn main() {
             100.0,
             0
         )
-    ));
+    ));*/
 
-    objects.push(Box::new(
+    /*objects.push(Box::new(
         Triangle::new(
             Vector3::new(-10.0, -2.0, -3.0),
             Vector3::new(10.0, -2.0, -3.0),
             Vector3::new(0.0, 5.0, -1.0),
+            0
+        )
+    ));*/
+
+    /*objects.push(Box::new(
+        Triangle::new(
+            Vector3::new(-10.0, -1.0, -3.0),
+            Vector3::new(10.0, -1.0, -3.0),
+            Vector3::new(0.0, 1.0, -1.0),
+            0
+        )
+    ));*/
+
+    /*objects.push(Box::new(
+    Triangle::new(
+            Vector3::new(-10.0, -1.0, -3.0),
+            Vector3::new(10.0, -1.0, -3.0),
+            Vector3::new(0.0, 1.0, -1.0),
+            0
+        )
+    ));*/
+    //0.700000,-1.200000,0.000000
+    //0.605600,-1.200000,-0.355700
+    //0.598800,-1.243700,-0.351700
+
+    objects.push(Box::new(
+        Triangle::new(
+            Vector3::new(-1.0, 0.0, -1.0) * 5.0,
+            Vector3::new(1.0, 0.0, -1.0) * 5.0,
+            Vector3::new(0.0, 2.0, -1.0) * 5.0,
             0
         )
     ));
